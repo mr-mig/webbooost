8d98daf58b95e5e92b90cfb9cb7fe249b57c82a2
Mr_Mig <alexey.migutsky@gmail.com>
2014-03-19 13:47:59 +0200

    remove twitter button

--- page_action/pageAction.js
+++ page_action/pageAction.js
@@ -8,19 +8,6 @@ chrome.tabs.getSelected(null, function(tab){
 	$id("time-count").innerHTML = stat.time/1000 + ' sec.';
 	$id("total-time-count").innerHTML = stats.totalTimeSaved/1000 + ' sec.';
 	document.title = "I have saved " + stats.totalTimeSaved/1000 + ' seconds while surfing the Web!';
-	
-	appendScript("/injectees/twitter/widgets.js", function(){
-		appendScript("/injectees/twitter/client.js", function(){
-			twttr.verifyCSP(false);
-
-			twttr.events.bind('click', function(){
-				ga.social("tweet", "twitter",  "/stats"); 
-			})	
-
-		});
-	});
-	
-
 });
 
 ga_ready(function(){
