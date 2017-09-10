1d165bc4fcbeb7ff597dc7efd3cf793683f50167
Mr_Mig <alexey.migutsky@gmail.com>
2017-09-10 18:42:58 +0200

    Ported regCheck

---
+++ src/checkers/regCheck.ts
@@ -0,0 +1,37 @@
+import { ParsedURL, RegExpMap } from '../domain'
+import { redirect, ALLOW_REQUEST_TOKEN } from '../requestInterceptor'
+
+// todo import real filters
+const regexpMap = {
+  'pattern$': 'filepath'
+} as RegExpMap
+
+let regexps: RegExp[] = []
+let substitutions: string[] = []
+
+// prebuild regexps
+Object.keys(regexpMap).forEach( key => {
+  regexps.push[new RegExp(key)]
+  substitutions.push(regexpMap[key])
+})
+
+export const check = (parsedURL: ParsedURL, tabId: number) => {
+  if (parsedURL.isExtension) return
+
+  const uri = parsedURL.uri
+  let substitution
+
+  for (let i = 0; i < regexps.length; i++){
+    if (regexps[i].test(uri)){
+      substitution = substitutions[i]
+      break
+    }
+  }
+
+  if (substitution){
+    parsedURL.boostedBy = 'reg'
+    redirect(substitution, tabId, parsedURL)
+  } else {
+    return ALLOW_REQUEST_TOKEN
+  }
+}
--- src/domain.ts
+++ src/domain.ts
@@ -2,6 +2,26 @@ import { URL } from './url'
 export { URL, ParsedURL } from './url'
 export type InjecteesFilepath = string
 
+// string pattern for URLs with substitutions
+type Pattern = string
+type RegexpPattern = string
+
+export type HashFilterManifest = {
+  hash: {
+    urls: Pattern[]
+  }
+}
+
+export type RegFilterManifest = {
+  reg?: {
+    pattern: RegexpPattern
+  }
+}
+
+export type FilterManifest = HashFilterManifest
+  | RegFilterManifest
+  | HashFilterManifest & RegFilterManifest
+
 export type ResourceDescriptor = {
   versions?: string[],
   urls: URL[],
@@ -10,6 +30,10 @@ export type ResourceDescriptor = {
 
 export type ResourceMap = {
   [resourceCDNURI in URL]: InjecteesFilepath | boolean
+}
+
+export type RegExpMap = {
+  [regexpPattern in RegexpPattern]: InjecteesFilepath
 }
 
 type HashConfig = {
